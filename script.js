9635a6c394c613e637b65f4bd05dd068dcbe1aa4
nakul71 <nakul.71@icloud.com>
2024-04-07 02:26:12 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var loadCount = document.querySelector("#line1-part1 h5");
+function loaderAnimation(){
+    var loadCount = document.querySelector("#line1-part1 h5");
 var tl = gsap.timeline();
 var c = 0;
 
@@ -26,3 +27,43 @@ tl.to("#loader" , {
     delay:3,
     duration:0.45,
 })
+
+tl.from("#page1",{
+    y:1200,
+    // opacity:1,
+})
+
+tl.to("#loader" , {
+    display:"none",
+})
+
+// tl.from("#head h1, #head3-left" ,{
+//     y:120,
+//     stagger:0.1,
+// })
+}
+
+function cursAnime(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#curs",{
+            left:dets.x,
+            top:dets.y,
+        })
+    })
+    
+    Shery.makeMagnet(".menu-opener__square" /* Element to target.*/, {
+        //Parameters are optional.
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 1,
+    });
+    
+    Shery.makeMagnet("#nav-right h4" /* Element to target.*/, {
+        //Parameters are optional.
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 1,
+    });
+}
+
+loaderAnimation();
+
+cursAnime();
